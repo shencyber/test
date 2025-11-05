9467c294aba7694b2280877ad238a18ee97c1baf
shencyber <1149683504@qq.com>
2025-11-05 23:16:12 +0800

    Add files via upload

--- component/NavComponent.js
+++ component/NavComponent.js
@@ -71,7 +71,7 @@ Vue.component('nav-component', {
         .nav-wrapper {
           width: 100%;
           max-width: 1280px;
-          margin: 0 auto 40px;
+          
           position: relative;
         }
 
